43e2be7e18ea122d5e141eccfe11641f3ea66d70
chakradhar47 <35587305+chakradhar47@users.noreply.github.com>
2022-01-04 14:56:58 +0530

    Add files via upload

---
+++ solid_rectangle_pattern.py
@@ -0,0 +1,6 @@
+a=int(input())
+b=int(input())
+counter = 0
+while counter < a:
+  print("* " * b)
+  counter = counter + 1
---
+++ solid_rightangled_triangle.py
@@ -0,0 +1,6 @@
+a=int(input())
+count=0
+while count<a:
+    b=("* "*(count+1))
+    print(b)
+    count=(count+1)
